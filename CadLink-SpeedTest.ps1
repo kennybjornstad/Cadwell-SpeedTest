80568fd8a32d6a008f4057a37c83ab2eae220e35
kennybjornstad <100637537+kennybjornstad@users.noreply.github.com>
2022-10-17 17:21:17 -0700

    Update CadLink-SpeedTest.ps1

--- CadLink-SpeedTest.ps1
+++ CadLink-SpeedTest.ps1
@@ -1,3 +1,4 @@
+# Uncomment this if scriptblock to force running as administrator and bypass local execution policy
 #if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) 
 #{ 
 #    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
